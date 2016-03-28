95f651dcbdc41655381d4362bbdf26b59910cf8a
Aleksa Sarai <cyphar@cyphar.com>
2016-03-28 11:41:51 +1100

    *: Cyphar -> Aleksa Sarai
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- redone/constants.py
+++ redone/constants.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # redone: A correct regex implementation in Python
-# Copyright (C) 2014 Cyphar
+# Copyright (C) 2014 Aleksa Sarai
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,6 +9,5 @@
 import string
 
 ALPHABET = set(string.printable)
-#METACHARS = {"^", ".", "*", "+", "?", "(", ")", "[", "]", "{", "}", "|", "\\"}
 METACHARS = {"^", ".", "*", "+", "?", "(", ")", "[", "]", "{", "}", "|", "\\"}
 SETMETA = {"[", "]", "\\"}
